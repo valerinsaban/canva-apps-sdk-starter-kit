77b252eb97c25ad8c2448a8feef3c74a3e455367
Valerin Saban <valerinsaban24@icloud.com>
2024-08-23 18:45:24 -0600

    Env APP

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,48 @@
 import { Button, Rows, Text } from "@canva/app-ui-kit";
 import { addNativeElement } from "@canva/design";
+import React from "react";
 import styles from "styles/components.css";
 
 export const App = () => {
   const onClick = () => {
     addNativeElement({
       type: "TEXT",
-      children: ["Hello world!"],
+      children: ["Hola Ody!"],
+    });
+    addNativeElement({
+      type: "TEXT",
+      children: ["Como estas?"],
     });
   };
+
+  const [state, setState] = React.useState({
+    cotizacion_id: "3"
+  });
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setState((prevState) => {
+      return {
+        ...prevState,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
 
   return (
     <div className={styles.scrollContainer}>
       <Rows spacing="2u">
         <Text>
-          To make changes to this app, edit the <code>src/app.tsx</code> file,
-          then close and reopen the app in the editor to preview the changes.
+          Ingresa el ID de Cotizacion
         </Text>
+        <input
+          type="text"
+          name="cotizacion_id"
+          value={state.cotizacion_id}
+          placeholder="Color #1"
+          onChange={handleChange}
+        />
         <Button variant="primary" onClick={onClick} stretch>
-          Do something cool
+          Generar Cotizacion
         </Button>
       </Rows>
     </div>
