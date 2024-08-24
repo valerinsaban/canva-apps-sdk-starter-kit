3629c084509d56bcee3fd1563ba4bd84e18d633f
Valerin Saban <valerinsaban24@icloud.com>
2024-08-23 21:34:40 -0600

    aPP

--- src/app.tsx
+++ src/app.tsx
@@ -1,45 +1,68 @@
-import { Button, Rows, Text } from "@canva/app-ui-kit";
+import {
+  Button,
+  FormField,
+  Rows,
+  Select,
+  Text,
+  TextInput,
+} from "@canva/app-ui-kit";
 import { addNativeElement } from "@canva/design";
-import React from "react";
 import styles from "styles/components.css";
+import axios from "axios";
+
+const params = {
+  contacto: "",
+  concepto: "Sitio Web (Catalogo)",
+  empresa: "",
+}
+
+const response = await axios.get("https://api.kairosgt.com/empresas", {
+  headers: {
+    "Authorization": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJub21icmUiOiJLYWlyb3MiLCJhcGVsbGlkbyI6IkdUIiwiY29ycmVvIjoiaW5mb0BrYWlyb3NndC5jb20iLCJ1c3VhcmlvIjoiYWRtaW4iLCJjbGF2ZSI6InNoYTEkYjc3N2VhNzckMSRlNDE4YzYzMmZmZDAxNzMxNGFmYWIyZGEwMzRlODBlNzEwZDNhM2Q4IiwiYWNjZXNvIjoiMSIsInJvbF9pZCI6MSwiY3JlYXRlZEF0IjoiMjAyMy0xMi0xM1QxODozNTo0NS4wMDBaIiwidXBkYXRlZEF0IjoiMjAyNC0wNS0yMlQxNzozMTozNS4wMDBaIn0sImlhdCI6MTcyNDQ2MTk0MSwiZXhwIjoxNzI0NTA1MTQxfQ.h8G_mVQek8keChrKl0sQQ5rQwoh0Dk_ZPPu4mbZnaXk",
+  },
+});
+
+let empresas: any = [];
+for (let i = 0; i < response.data.data.length; i++) {
+  empresas.push({
+    value: response.data.data[i].nombre,
+    label: response.data.data[i].nombre
+  });
+}
 
 export const App = () => {
-  const onClick = () => {
+  const onClick = async () => {
     addNativeElement({
       type: "TEXT",
-      children: ["Hola Ody!"],
-    });
-    addNativeElement({
-      type: "TEXT",
-      children: ["Como estas?"],
+      children: [params.empresa],
+      top: 132,
+      left: 155,
+      textAlign: "center",
+      fontSize: 20,
+      fontWeight: "bold",
+      color: "#000000",
+      width: 300,
+      rotation: 0,
     });
   };
-
-  const [state, setState] = React.useState({
-    cotizacion_id: "3"
-  });
-
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setState((prevState) => {
-      return {
-        ...prevState,
-        [event.target.name]: event.target.value,
-      };
-    });
-  }
 
   return (
     <div className={styles.scrollContainer}>
       <Rows spacing="2u">
         <Text>
-          Ingresa el ID de Cotizacion
+          Selecciona la empresa
         </Text>
-        <input
-          type="text"
-          name="cotizacion_id"
-          value={state.cotizacion_id}
-          placeholder="Color #1"
-          onChange={handleChange}
+        <FormField
+          label="Form field label"          
+          control={(props) => (
+            <Select
+              {...props} // <--- pass props down id, value and error to connect Select component to FormField
+              options={empresas}
+              onChange={(value) => {
+                params.empresa = value;
+              }}
+            />
+          )}
         />
         <Button variant="primary" onClick={onClick} stretch>
           Generar Cotizacion
